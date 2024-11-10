b2e7824c7d7302f84d0b85faa4c06076d5389168
Fernando Armonelli Fiedler <fernandoarmonelli@gmail.com>
2024-11-10 20:48:20 -0300

    update on clases

--- clase--01/cookies/src/app.js
+++ clase--01/cookies/src/app.js
@@ -1,36 +1,26 @@
-// Import necessary modules
-import express from "express"; // Express.js framework is used to handle HTTP requests
-import handlebars from "express-handlebars"; // Handlebars templating engine for Express.js
-import _dirname from "./dirname.js"; // Path to the current directory
-import viewRoutes from "./routes/views.routes.js"; // Custom routing module
+import express from "express";
+import handlebars from "express-handlebars";
+import _dirname from "./dirname.js";
+import viewRoutes from "./routes/views.routes.js";
+import cookieParser from "cookie-parser";
 
-const PORT = 8080; // Port number on which the server will listen
-
-// Create an instance of Express application
+const PORT = 8080;
 const app = express();
 
-// Middleware that parses incoming request bodies in a middleware before your handlers, available under the req.body property.
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Middleware that parses incoming requests with JSON payloads
 app.use(express.json());
 
-// Setting up Handlebars as template engine
-app.engine("handlebars", handlebars({defaultLayout: 'main'}));
-
-// Set path where views (templates) are stored
+app.engine("handlebars", handlebars.engine());
 app.set("views", _dirname + "/views");
-
-// Set default view (template) engine
 app.set("view engine", "handlebars");
-
-// Serving static files from 'public' directory
 app.use(express.static("public"));
+app.use(cookieParser("secretKey"));
 
-// Use custom routes for handling requests
+
+
 app.use("/", viewRoutes);
 
-// Start the server and listen on specified port
 app.listen(PORT, () => {
-  console.log(`Server on port ${PORT}`); // Logs the running port after successful connection
+  console.log(`Server on port ${PORT}`);
 });
