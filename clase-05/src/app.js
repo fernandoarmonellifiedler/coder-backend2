9ec5acd6dfde03df8cda909b2fecd1610bd51dd4
Fernando Armonelli Fiedler <fernandoarmonelli@gmail.com>
2024-11-10 21:29:12 -0300

    create routes

--- clase-05/src/app.js
+++ clase-05/src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { connectDB } from "../config/mongoDB.config.js";
+import router from "./router/index.router.js";
 
 const app = express();
 connectDB();
+
+app.use("/api", router);
 
 app.listen(8080, () => {
     console.log("server on port 8080");
