dbf106bf477714c3701009b27e07e2278e3ddf1b
Fernando Armonelli Fiedler <fernandoarmonelli@gmail.com>
2024-11-10 22:16:23 -0300

    create register for user

--- clase-05/src/router/user.router.js
+++ clase-05/src/router/user.router.js
@@ -5,4 +5,19 @@ const router = Router();
 
 router.use("/user", userRouter());
 
+router.post("/register", async (req, res) => {
+    try {
+
+        //const { name, lastName, email, password } = req.body;
+
+        const user = await userDao.create(req.body);
+
+        res.status(201).json({ status: "ok", payload: user })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ status: "error", msg: "Internal Server Error" });
+    }
+});
+
 export default router;
