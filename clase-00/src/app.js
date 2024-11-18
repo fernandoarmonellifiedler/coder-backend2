04d8e3ab531f5f362f050fee0414d1eebb31f6d9
Fernando Armonelli Fiedler <fernandoarmonelli@gmail.com>
2024-11-18 11:56:43 -0300

    fix code to connect t database

--- clase-00/src/app.js
+++ clase-00/src/app.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 import router from './router/index.router.js';
 
 const app = express();
-mongoose.connect('mongodb://localhost:27017/clase00');
+mongoose.connect('mongodb+srv://fernandoarmonelli:EGDFjlF1Twc9QOuW@cluster0.xih0z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 console.log('Mongo db connect');
-console.log(localhost);
+
 // Middleware para manejar JSON
 app.use(express.json());
 
