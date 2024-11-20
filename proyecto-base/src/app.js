7ccf9f46894aeb8f36f804118d8ea9943a4fe427
Fernando Armonelli Fiedler <fernandoarmonelli@gmail.com>
2024-11-20 20:13:58 -0300

    remove views and handlebars from project

--- proyecto-base/src/app.js
+++ proyecto-base/src/app.js
@@ -3,7 +3,6 @@ import routes from "./routes/index.js";
 import __dirname from "./dirname.js";
 import handlebars from "express-handlebars";
 import { Server } from "socket.io";
-import viewsRoutes from "./routes/views.routes.js";
 import { connectMongoDB } from "./config/mongoDB.config.js";
 
 const app = express();
@@ -12,16 +11,10 @@ connectMongoDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.engine("handlebars", handlebars.engine()); // Inicia el motor del la plantilla
-app.set("views", __dirname + "/views"); // Indicamos que ruta se encuentras las vistas
-app.set("view engine", "handlebars"); // Indicamos con que motor vamos a utilizar las vistas
 app.use(express.static("public"));
 
 // Rutas de la api
 app.use("/api", routes);
-
-// Ruta de las vistas
-app.use("/", viewsRoutes)
 
 const httpServer = app.listen(8080, () => {
   console.log("Servidor escuchando en el puerto 8080");
