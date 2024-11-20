2b61e324500d57c0c76e067ad4663db726fe5049
Fernando Armonelli Fiedler <fernandoarmonelli@gmail.com>
2024-11-19 23:53:28 -0300

    remove other views files

--- clase-07/src/app.js
+++ clase-07/src/app.js
@@ -15,9 +15,6 @@ initializePassport();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.engine("handlebars", handlebars.engine()); // Inicia el motor del la plantilla
-app.set("views", __dirname + "/views"); // Indicamos que ruta se encuentras las vistas
-app.set("view engine", "handlebars"); // Indicamos con que motor vamos a utilizar las vistas
 app.use(express.static("public"));
 app.use(
   session({
