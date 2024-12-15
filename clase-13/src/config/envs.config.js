d4ea33490c30da2b1a101e9ac34fc057d1a8dff7
Fernando Armonelli Fiedler <fernandoarmonelli@gmail.com>
2024-12-15 20:38:31 -0300

    add mailing service

--- clase-13/src/config/envs.config.js
+++ clase-13/src/config/envs.config.js
@@ -9,4 +9,5 @@ export default {
   JWT_KEY: process.env.JWT_KEY,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GMAIL_PASS: process.env.GMAIL_PASS
 };
